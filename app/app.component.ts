4d3a41afe58116dc2f9cc77cfc628f13fdb75288
Todd Nichols <nivram80@gmail.com>
2016-10-05 22:14:39 -0500

    make app component more generic for seed functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>OmahaAreaMan</h1>'
+    template: '<h1>Angular 2 TypeScript Seed Project</h1>'
 })
 
 export class AppComponent {
